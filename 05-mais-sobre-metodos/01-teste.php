34c0017f392ed721068870eea0279519b0c3d530
Gabriel Marques <bielmarques922@gmail.com>
2022-08-12 15:34:58 -0300

    Introdução a POO - Modulo 05 - Método construtor

---
+++ 05-mais-sobre-metodos/01-teste.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'src/Conta.php';
+
+$primeiraConta = new Conta('123.456.789-10', 'Gabriel Marques');
+$primeiraConta->deposita(500);
+$primeiraConta->saca(300);
+
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+
+
+$segundaConta = new Conta('987.654.321-11', 'Vinicius Dias');
+$segundaConta->deposita(500);
+
+echo $segundaConta->recuperaNomeTitular() . PHP_EOL;
+echo $segundaConta->recuperaCpfTitular() . PHP_EOL;
+echo $segundaConta->recuperaSaldo() . PHP_EOL;
